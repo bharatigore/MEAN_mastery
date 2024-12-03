91a3f51b5b40ee3c3e007577cf088adaaa51a19c
bharatigore <bhartigore24@gmail.com>
2024-12-03 16:53:50 +0530

    Study concept of inline

---
+++ Angular-projects/inline/src/app/ppa/ppa.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ppa',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ppa works!
+      
+    </p>
+    
+  `,
+  styles: ``
+})
+export class PPAComponent {
+
+}
